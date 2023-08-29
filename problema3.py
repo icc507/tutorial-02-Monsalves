3995b02c1bee650fd206d4c1f0dfc88eaa4e8987
Monsalves <105566947+Monsalves@users.noreply.github.com>
2023-08-29 10:22:14 -0400

    Update problema3.py

--- problema3.py
+++ problema3.py
@@ -9,5 +9,34 @@
 #         20 30 90 90 8 5 90
 #La salida debe ser
 #         [20, [8, [5, [], [], []], [], []], [], [30, [], [], [90, [], [90, [], [90, [], [], []], []], []]]]
-t = input()
-print(t)
+t = input().split()
+
+for i in range(len(t)):
+    try:
+        t[i] = int(t[i])
+    except ValueError:
+        continue
+
+
+#Árbol binario con listas
+
+def arbolBinario(numero):
+	return [numero, [],[],[]]
+
+def insertaEnArbolBinario(arbol,numero):
+	if arbol==[]:
+		arbol+=arbolBinario(numero)
+	elif numero == arbol[0]:
+		insertaEnArbolBinario(arbol[2],numero)
+	elif numero <arbol[0]:
+		insertaEnArbolBinario(arbol[1],numero)
+	else:
+		insertaEnArbolBinario(arbol[3],numero)
+
+w = arbolBinario(t[0])
+t.pop(0)
+for i in range(len(t)):
+	insertaEnArbolBinario(w,t[i])
+
+print(w)
+
