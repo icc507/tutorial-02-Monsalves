eafdb58c8b016babc7484544fe282665c4ac07cf
Monsalves <105566947+Monsalves@users.noreply.github.com>
2023-08-29 09:12:37 -0400

    Update problema1.py

--- problema1.py
+++ problema1.py
@@ -9,6 +9,19 @@
 #		  mundo 44
 #La salida debe ser
 #         ('mundo', 44, 20, 90, 'hola', 'mundo', 44)
-t = tuple(input().split())
-m = tuple(input().split())
-print(m+t+m)
+t = input().split()
+m = input().split()
+for i in range(len(t)):
+    try:
+        t[i] = int(t[i])
+    except ValueError:
+        continue
+
+for i in range(len(m)):
+    try:
+        m[i] = int(m[i])
+    except ValueError:
+        continue
+
+
+print(tuple(m)+tuple(t)+tuple(m))
